f0f0b9a565ece98423af3a1c7667a045b6e934c7
CyprianESPI <cyprianespinoza@gmail.com>
2023-12-09 21:20:37 +0100

    Update tabs and add footer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const content = document.getElementById("content");
 
+let TabIndex = 0;
 let Tabs = 0;
 
 /* Tab buttons */
@@ -11,12 +12,25 @@ function CreateTabButton(name) {
     Tabs++;
     const tab_index = Tabs - 1;
     elem.addEventListener("click", () => {
-        console.log(tab_index);
+        console.log(tab_index, name);
+        main.innerText = name;
     });
 
     return elem;
 }
 
-content.appendChild(CreateTabButton("Home"));
-content.appendChild(CreateTabButton("Menu"));
-content.appendChild(CreateTabButton("Contact"));
+const tabs = document.createElement("div");
+tabs.appendChild(CreateTabButton("Home"));
+tabs.appendChild(CreateTabButton("Menu"));
+tabs.appendChild(CreateTabButton("Contact"));
+content.appendChild(tabs);
+
+const main = document.createElement("main");
+content.appendChild(main);
+
+const footer = document.createElement("h2");
+const footer_link = document.createElement("a");
+footer_link.href = "https://www.theodinproject.com/lessons/node-path-javascript-restaurant-page";
+footer_link.innerText = "The Odin Project | Project: Restaurant Page";
+footer.appendChild(footer_link);
+content.appendChild(footer);
